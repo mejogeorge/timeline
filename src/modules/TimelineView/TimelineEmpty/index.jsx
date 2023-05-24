c500addb3f3c3ed58108559abb83f18f1e25b35f
Mejo <mejo@192.168.68.100>
2023-05-24 21:09:01 +0530

    text fix

--- src/modules/TimelineView/TimelineEmpty/index.jsx
+++ src/modules/TimelineView/TimelineEmpty/index.jsx
@@ -2,6 +2,6 @@ import styles from './TimelineEmpty.module.scss'
 
 export default () => (
   <div className={styles.wrapper}>
-    Please add a Add new Group or Load Sample Data to view the calendar.
+    Please add a new Group or Load Sample Data to view the calendar timeline.
   </div>
 )
